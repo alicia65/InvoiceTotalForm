94c1cfb10b574c9889745d92244e46ac9c460e40
alicia65 <alicia65>
2020-01-29 11:26:25 -0600

    Add project files.

---
+++ InvoiceTotalForm/frmInvoiceTotal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvoiceTotalForm
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_ContextMenuStripChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+
+        {
+            decimal subtotal = Convert.ToDecimal(txtSubtotal.Text);//Convert subtotal to decimal
+            decimal discountPercent = 0m;
+            if (subtotal >= 500)//loop through different subtotals and discountPercents
+            {
+                discountPercent = .2m;
+            }
+            else if (subtotal >= 250 && subtotal < 500)
+            {
+                discountPercent = .15m;
+            }
+            else if (subtotal >= 100 && subtotal < 250)
+            {
+                discountPercent = .1m;
+            }
+
+            decimal discountAmount = subtotal * discountPercent;//Calculate discountAmount by multiplication
+            decimal invoiceTotal = subtotal - discountAmount;
+
+            txtDiscountPercent.Text = discountPercent.ToString("p1");// print text in percent
+            txtDiscountAmount.Text = discountAmount.ToString("c");// print text in currency 
+            txtTotal.Text = invoiceTotal.ToString("c");
+
+            txtSubtotal.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();//to end program
+        }
+    }
+}     
+
